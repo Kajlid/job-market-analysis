7e614501b250d8a8f6378b096e8fef886edd4c72
Kajlid <104261817+Kajlid@users.noreply.github.com>
2025-10-02 15:23:24 +0200

    feat(scripts): do simple analysis of data in pandas

---
+++ scripts/eda.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+
+df = pd.read_json('data/data.json')
+# print(df.sample(5))
+
+print(df.loc[:, "driving_licence"])
+
+# print(df.info())
+
+# print(df.describe())
