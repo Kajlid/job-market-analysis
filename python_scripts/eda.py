d74167759bec394cc7cb7411dc322080880a1dfe
Kajlid <104261817+Kajlid@users.noreply.github.com>
2025-10-03 09:51:38 +0200

    feat(eda): primary exploratory data analysis in python

--- python_scripts/eda.py
+++ python_scripts/eda.py
@@ -7,8 +7,12 @@ warnings.filterwarnings('ignore')
 df = pd.read_json('data/data.json')
 # print(df.sample(5))
 
-print(df.loc[:, "driving_licence"])
+# print(df.loc[:, "driving_licence"])
 
 # print(df.info())
+
+# print(df.loc[:, "number_of_vacancies"])
+
+print(df["workplace_address"].str["municipality"])
 
 # print(df.describe())
