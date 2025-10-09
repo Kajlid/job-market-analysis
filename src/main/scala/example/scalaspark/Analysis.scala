d4bee8755b5ed13f0db09fb47427ff2071afd023
Kajlid <104261817+Kajlid@users.noreply.github.com>
2025-10-09 16:17:08 +0200

    Create new function for creating embeddings for clustering (does not work because of package error)

--- src/main/scala/example/scalaspark/Analysis.scala
+++ src/main/scala/example/scalaspark/Analysis.scala
@@ -1,24 +1,53 @@
 package com.example.scalaspark
 
-import org.apache.spark.sql.DataFrame        // DataSet?
-import org.apache.spark.sql.functions.{avg, col, count, lit}
-import com.johnsnowlabs.nlp.base.DocumentAssembler
-
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.{avg, col, count, lit, concat_ws}
+import com.johnsnowlabs.nlp.base._
+import com.johnsnowlabs.nlp.annotator._
+import com.johnsnowlabs.nlp.embeddings.XlmRoBertaEmbeddings
+import org.apache.spark.ml.Pipeline
 
 object Analysis {
+
   def calculateAverageNumberOfVacancies(data: DataFrame): DataFrame = {
     val result = data
       .groupBy(col("workplace_address").getField("municipality").as("municipality"))
       .agg(
-        avg("number_of_vacancies"),
+        avg("number_of_vacancies").as("avg_number_of_vacancies"),
         count(lit(1)).as("num_vacancies_per_municipality")
       )
 
-    result.sort(col("num_vacancies_per_municipality").desc).write.option("header", "true").csv("output/municipal_vacancies")
-    result
+    val sortedResult = result.sort(col("num_vacancies_per_municipality").desc)
+    sortedResult.write.option("header", "true").csv("output/municipal_vacancies")
+    sortedResult
   }
 
-  def assembleJobDescriptions(data: DataFrame, textCol: String = "job_description"): DataFrame = {
-    val documentAssembler = new DocumentAssembler()
+  def createEmbeddingsForJobAds(data: DataFrame, textCols: Seq[String] = Seq("headline", "description", "employer")): DataFrame = {
+    // needed for clustering
+      val combinedData = if (textCols.length > 1) {
+      val combinedCol = concat_ws(" ", textCols.map(col): _*)
+      data.withColumn("combined_text", combinedCol)
+        } else {
+          data.withColumn("combined_text", col(textCols.head))
+        }
+
+      // Document Assembler
+      val documentAssembler = new DocumentAssembler()
+        .setInputCol("combined_text")
+        .setOutputCol("document")
+
+      // Embeddings
+      val embeddings = XlmRoBertaEmbeddings.pretrained("xlm-roberta-base", "xx") // xx = multilingual
+        .setInputCols("document")
+        .setOutputCol("document_embeddings")
+
+        val pipeline = new Pipeline().setStages(Array(
+        documentAssembler,
+        embeddings
+      ))
+    val model = pipeline.fit(combinedData)
+    val dataWithEmbeddings = model.transform(combinedData)
+    dataWithEmbeddings
   }
+
 }
