fb50c40e45f1c743ae65420678858ffd7968fdbc
Kajlid <104261817+Kajlid@users.noreply.github.com>
2025-10-18 16:46:13 +0200

    Cleaned final version

--- python_scripts/job_analysis.py
+++ python_scripts/job_analysis.py
@@ -24,9 +24,6 @@ MONGO_USER = os.getenv("MONGO_USER")
 MONGO_PASS = os.getenv("MONGO_PASS")
 MONGO_HOST = os.getenv("MONGO_HOST")
 MONGO_DB = os.getenv("MONGO_DB")
-MONGO_COLLECTION = os.getenv("MONGO_COLLECTION")
-HDFS_USER = os.getenv("HDFS_USER")
-HDFS_PATH = os.getenv("HDFS_PATH")
 
 # Construct Mongo URI
 mongo_uri = f"mongodb+srv://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}/{MONGO_DB}?retryWrites=true&w=majority"
